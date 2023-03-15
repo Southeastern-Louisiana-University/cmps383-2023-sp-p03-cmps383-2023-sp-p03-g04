6dc37b41cc1671fe408dccd575226164ef78b712
kaiihayes <kaitlyn.hayes-2@selu.edu>
2023-03-15 15:42:07 -0500

    style changes

--- SP23-P03-Mobile/screens/login-screen/loginStyle.tsx
+++ SP23-P03-Mobile/screens/login-screen/loginStyle.tsx
@@ -1,20 +1,38 @@
 import { StyleSheet} from "react-native";
 
 const loginStyle=StyleSheet.create({
-    container: {
+    containerTitle: {
         flex: 1,
         justifyContent: 'center',
         padding: 24,
-        backgroundColor: '#fdba74',
+        backgroundColor: 'white',
       },
       title: {
-        marginTop: 16,
+        borderWidth: 4,
+        backgroundColor: "#fdba74",
+        marginTop: 30,
+        marginRight: 30,
+        marginLeft: 30,
         paddingVertical: 8,
         color: '#20232a',
         textAlign: 'center',
         fontSize: 30,
         fontWeight: 'bold',
       },
+
+      welcomeTitle: {
+        flex: 1,
+        padding: 74,
+        textAlign: 'center',
+        fontSize: 50,
+        fontWeight: "bold",
+      },
+
+      subCaption: {
+        padding: 20,
+        textAlign: "center",
+        fontSize: 20,
+      }
 });
 
 export default loginStyle;
