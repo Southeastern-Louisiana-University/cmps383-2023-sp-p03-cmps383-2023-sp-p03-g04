d873accbf101f4e858378a447d57a33a3bd3f15b
Samuel Echols <sechols@dbsysgraph.com>
2023-03-21 12:16:33 -0500

    Created Entities

---
+++ SP23.P03.Web/Features/Trains/Train.cs
@@ -0,0 +1,16 @@
+﻿using SP23.P03.Web.Features.Carriers;
+using SP23.P03.Web.Features.Trains;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SP23.P03.Web.Features
+{
+    public class Train
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [ForeignKey("CarrierId")]
+        public int CarrierId { get; set; }
+        public Carrier Carrier { get; set; }
+        public virtual ICollection<TrainCar> TrainCars { get; set; }
+    }
+}
---
+++ SP23.P03.Web/Features/Trains/TrainCar.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SP23.P03.Web.Features.Trains
+{
+    public class TrainCar
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [ForeignKey("TrainId")]
+        public int TrainId { get; set; }
+        [ForeignKey("TrainCarTypeId")]
+        public int TrainCarTypeId { get; set; }
+        public TrainCarType TrainCarType { get; set; }
+        public int Capacity { get; set; }
+    }
+}
