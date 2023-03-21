31c8355f8a3c4016e3e0d8141e1057d316dd6bf7
Samuel Echols <sechols@dbsysgraph.com>
2023-03-21 12:40:24 -0500

    Created Carriers Controller

---
+++ SP23.P03.Web/Controllers/CarriersController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SP23.P03.Web.Data;
+using SP23.P03.Web.Features.Carriers;
+
+namespace SP23.P03.Web.Controllers
+{
+    [ApiController]
+    public class CarriersController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public CarriersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpPost("{carrier}")]
+        public async Task<ActionResult<Carrier>> CreateCarrier(Carrier carrier)
+        {
+            var newCarrier = carrier;
+            await _dataContext.AddAsync(newCarrier);
+
+            return CreatedAtAction(nameof(GetById), 
+                new { id = newCarrier.Id }, carrier);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Carrier>>> GetAllCarriers()
+        {
+            return await _dataContext.Carrier.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Carrier>> GetById(int id)
+        {
+            var carrier = await _dataContext.Carrier.FindAsync(id);
+
+            if (carrier == null)
+                return NotFound();
+            return Ok(carrier);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Carrier>> UpdateCarrier(int id, Carrier updateCarrier)
+        {
+            var carrier = await _dataContext.Carrier.FindAsync(id);
+
+            if (carrier == null)
+                return NotFound();
+
+            carrier.Name = updateCarrier.Name;
+
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(carrier);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCarrier(int id)
+        {
+            var carrier = await _dataContext.Carrier.FindAsync(id);
+
+            if (carrier == null)
+                return NotFound();
+            _dataContext.Carrier.Remove(carrier);
+            await _dataContext.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
