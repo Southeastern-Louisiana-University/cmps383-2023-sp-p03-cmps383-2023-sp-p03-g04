6f50fcf481b64f1238767ea1bccb4d7edc5d5c58
Samuel Echols <sechols@dbsysgraph.com>
2023-03-28 16:43:57 -0500

    Created TrainCarTypes dto and more

--- SP23.P03.Web/Features/Trains/TrainCar.cs
+++ SP23.P03.Web/Features/Trains/TrainCar.cs
@@ -8,9 +8,10 @@ namespace SP23.P03.Web.Features.Trains
         public int Id { get; set; }
         [ForeignKey("TrainId")]
         public int TrainId { get; set; }
+
         [ForeignKey("TrainCarTypeId")]
         public int TrainCarTypeId { get; set; }
-        public TrainCarType TrainCarType { get; set; }
+        public virtual TrainCarType TrainCarType { get; set; }
         public int Capacity { get; set; }
     }
 }
---
+++ SP23.P03.Web/Features/Trains/TrainDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SP23.P03.Web.Features.Trains
+{
+    public class TrainDto
+    {
+
+        public int CarrierId { get; set; }
+        public int Capacity { get; set; }
+    }
+}
