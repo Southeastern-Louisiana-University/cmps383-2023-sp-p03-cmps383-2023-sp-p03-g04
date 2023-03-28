ee76705f6d4566540a97da1a25bdd2b21b8796ea
Samuel Echols <sechols@dbsysgraph.com>
2023-03-28 17:14:14 -0500

    Added Migration

--- SP23.P03.Web/Features/Trains/TrainCar.cs
+++ SP23.P03.Web/Features/Trains/TrainCar.cs
@@ -8,10 +8,11 @@ namespace SP23.P03.Web.Features.Trains
         public int Id { get; set; }
         [ForeignKey("TrainId")]
         public int TrainId { get; set; }
+        public Train Train { get; set; }
 
         [ForeignKey("TrainCarTypeId")]
         public int TrainCarTypeId { get; set; }
-        public virtual TrainCarType TrainCarType { get; set; }
+        public TrainCarType TrainCarType { get; set; }
         public int Capacity { get; set; }
         public int TicketsSold { get; set; }
     }
