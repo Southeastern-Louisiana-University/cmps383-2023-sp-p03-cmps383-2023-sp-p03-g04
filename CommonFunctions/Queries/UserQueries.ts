fdc22baf0bf6c8b29cec4f2071de2c5a152b7a89
Samuel Echols <sechols@dbsysgraph.com>
2023-03-28 10:41:00 -0500

    Added UserQueries

---
+++ CommonFunctions/Queries/UserQueries.ts
@@ -0,0 +1,34 @@
+import { Api } from './../../SP23.P03.Web/ClientApp/src/Config';
+import { CreateUserRequest, GetUserResponse, User } from './../../CommonTypes/UserTypes';
+
+export function getCurrentUser(): User | null {
+    let result: User | null = null
+
+    async () => {
+        try {
+            const {data, status} = await Api.get<GetUserResponse>('/authentication/me')
+
+            if (status === 200) result = data.user;
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    return result;
+}
+
+export function createUser(user: CreateUserRequest): User | null {
+    let result: User | null = null
+
+    async () => {
+        try {
+            const {data, status} = await Api.post<GetUserResponse>('/users', user)
+
+            if (status === 200) result = data.user
+
+        } catch (error) {
+            console.error(error)
+            
+        }
+    }
+    return result;
+}
