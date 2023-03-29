6a4f6b8dfc356709401a229c53d436c9f73ac2ed
Autumn Kennedy <autumnkennedy0714@gmail.com>
2023-03-29 01:00:29 -0500

    removed console log

--- SP23-P03-Mobile/navigation/auth-navigator.tsx
+++ SP23-P03-Mobile/navigation/auth-navigator.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/login-screen/login-screen';
 import {ROUTES} from '../constants';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabNavigator from "../navigation/bottom-tab-navigator";
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 function AuthNavigator() {
-    console.log(Stack);
+   
   return (
     <NavigationContainer>
-    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
-      <Stack.Screen name={ROUTES.LOGIN} component={LoginScreen} />
-      <Stack.Screen name={ROUTES.HOME} component={BottomTabNavigator}/>
-    
-    </Stack.Navigator>
+    <Drawer.Navigator>
+      <Drawer.Screen name={ROUTES.LOGIN} component={LoginScreen} />
+      <Drawer.Screen name={ROUTES.HOME} component={BottomTabNavigator}/>
+    </Drawer.Navigator>
     </NavigationContainer>
   );
 }
 
 export default AuthNavigator;
+
