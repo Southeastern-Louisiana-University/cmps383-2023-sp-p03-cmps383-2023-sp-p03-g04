c6d69ffd86c8fa2f80aa4b42abe782b6a2933298
Samuel Echols <sechols@dbsysgraph.com>
2023-04-02 13:29:08 -0500

    Created UseData hook

---
+++ SP23.P03.Web/ClientApp/src/Hooks/UseData.ts
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, createContext, useContext } from "react";
+import { Api } from "../Config";
+
+export const useApiData = (endpoint: string) => {
+	const [data, setData] = useState<any | null | undefined>(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState<Error | null | undefined>(null);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await Api.get(endpoint);
+				setData(response.data);
+			} catch (error) {
+				setError(error as Error | null);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchData();
+	}, [endpoint]);
+
+	return { data, loading, error };
+};
