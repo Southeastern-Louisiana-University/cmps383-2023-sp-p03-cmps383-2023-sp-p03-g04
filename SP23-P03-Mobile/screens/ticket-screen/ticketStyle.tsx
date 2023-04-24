c7b5a0463d1ee2b0b5b0dede55a91b1573376623
kaiihayes <kaitlyn.hayes-2@selu.edu>
2023-04-23 21:56:57 -0500

    adding the tickets

--- SP23-P03-Mobile/screens/ticket-screen/ticketStyle.tsx
+++ SP23-P03-Mobile/screens/ticket-screen/ticketStyle.tsx
@@ -1,20 +1,51 @@
 import { StyleSheet} from "react-native";
 
 const ticketStyle=StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        padding: 24,
-        backgroundColor: '#ffffff',
-      },
-      title: {
-        marginTop: 10,
-        paddingVertical: 5,
-        color: '#20232a',
-        textAlign: 'center',
-        fontSize: 25,
-        fontWeight: 'bold',
-      },
+  title: {
+    marginTop: 20,
+    paddingVertical: 5,
+    color: '#5359d1',
+    textAlign: 'center',
+    fontSize: 25,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#f2f2f2",
+    paddingHorizontal: 30,
+    justifyContent: "center",
+    alignContent: "center",      
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    justifyContent: "center",
+    alignContent: "center",
+    marginLeft: 10,
+    color: '#FDBA74',
+    textTransform: 'uppercase',
+  },
+  infoContainer: {
+    flexDirection: "row",
+    marginVertical: 10,
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginRight: 10,
+    flex: 1,
+    color: '#555555',
+  },
+  value: {
+    fontSize: 18,
+    flex: 2,
+    textAlign: "right",
+    color: '#5359d1',
+  },
 });
 
 export default ticketStyle;
