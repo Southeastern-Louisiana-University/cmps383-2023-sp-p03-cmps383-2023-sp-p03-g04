73f2bd78010eb203a27e87a61329407f08ee80aa
Autumn Kennedy <autumnkennedy0714@gmail.com>
2023-04-23 23:19:11 -0500

    Auth screens load when logged in

--- SP23-P03-Mobile/navigation/app.nav.tsx
+++ SP23-P03-Mobile/navigation/app.nav.tsx
@@ -2,21 +2,49 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import { NativeBaseProvider } from 'native-base';
 import TopAppBar from './top-app-bar';
+import AuthTopAppBar from './auth-top-app-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import BottomTabNavigator from './bottom-tab-navigator';
+import AuthBottomTabNavigator from './auth-bottom-tab-navigator';
+import { getCurrentUser } from '../data/queries/user-queries';
 
 const AppNav = () => {
-    return(
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <NativeBaseProvider>
-        
-          <TopAppBar/>
-          {/*<AuthTopAppBar/>*/}
-          <BottomTabNavigator/>
-         {/*<AuthBottomTabNavigator/>*/}
-      </NativeBaseProvider>
-    </GestureHandlerRootView>
-    )
-}
+    const currentUser = getCurrentUser(); 
+  
+    const renderAuth = () => {
+      if (currentUser === null) {
+        console.log(currentUser);
+        return (
+          <>
+            <AuthTopAppBar />
+            <AuthBottomTabNavigator />
+          </>
+        );
+      } else {
+        console.log('NULL');
+        return( 
+            <>
+            <TopAppBar />
+            <BottomTabNavigator />
+         </>
+        )
+      }
+    };
+  
+    return (
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <NativeBaseProvider>
+          {renderAuth()}
+        </NativeBaseProvider>
+      </GestureHandlerRootView>
+    );
+  };
+  
+  export default AppNav;
 
-export default AppNav;
+  
+  
+  
+  
+  
+  
